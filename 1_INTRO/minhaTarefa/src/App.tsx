3f86e3bef2a3d760936c02bcf0ee168bc537d22d
Alexandre Coletti <eu_axil@yahoo.com.br>
2024-01-16 10:42:58 -0300

    finalizando sessao 1

---
+++ 1_INTRO/minhaTarefa/src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1>Vite + React</h1>
+      <div>
+        <p className='meuParagrafo'>Meu paragrafo!!!!!!</p>
+      </div>
+    </>
+  )
+}
+
+export default App
