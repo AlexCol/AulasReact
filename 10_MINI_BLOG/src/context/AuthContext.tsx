05f9a33b5cf7c6cc86a58aae61d101679d21a67b
Alexandre Coletti <eu_axil@yahoo.com.br>
2024-02-01 19:54:10 -0300

    adicionando funcionalidade de contagem de abas abertas e dar logout quando a ultima for fechada

--- 10_MINI_BLOG/src/context/AuthContext.tsx
+++ 10_MINI_BLOG/src/context/AuthContext.tsx
@@ -1,6 +1,7 @@
 import { User } from "firebase/auth";
 import { ReactNode, createContext, useContext, useEffect } from "react";
 import { useAuthentication } from "../Hooks/useAuthentication";
+import Cookies from 'js-cookie';
 
 export const AuthContext = createContext<User|null|undefined>(null);
 
@@ -8,20 +9,51 @@ export function AuthProvider(
     { children, value }: { children: ReactNode, value: User|null|undefined } 
 ) {
     const {logout} = useAuthentication();
-    
-    //+realizar ultima ação antes de fechar, realizar o logout papra limpeza de tokens
-  // Tipando o 'event' como BeforeUnloadEvent
-    useEffect(() => {
-        const handleTabClose = (event: Event) => {
-            event.preventDefault();
-            logout();
-        };
-        window.addEventListener("unload", handleTabClose);
 
+    //+realizar contagem de paginas abertas, se fechar todas, dar logout    
+    //!metodo usando cookies
+    useEffect(() => {
+        let numSessions = parseInt(Cookies.get('sessions') || '0', 10) + 1;
+        Cookies.set('sessions', numSessions.toString());
+        console.log(numSessions);
+    
+        const handleTabClose = () => {
+          let numSessions = parseInt(Cookies.get('sessions') || '0', 10) - 1;
+          Cookies.set('sessions', numSessions.toString());
+          console.log(numSessions);
+          if (numSessions <= 0) {
+            logout();
+            Cookies.set('sessions', '0');
+          }
+        };
+    
+        window.onunload = handleTabClose;
         return () => {
-        window.removeEventListener("unload", handleTabClose);
-        }
-    }, []);
+          handleTabClose();
+        };
+      }, []);
+    
+    
+    //!modo usando localstorage
+    // useEffect(() => {
+    //     let numSessions = parseInt(localStorage.getItem("sessions")||"0") + 1;
+    //     localStorage.setItem('sessions', numSessions.toString());
+    //     console.log(numSessions)
+
+    //     const handleTabClose = () => {            
+    //         let numSessions = parseInt(localStorage.getItem("sessions")||"0") - 1;
+    //         localStorage.setItem('sessions', numSessions.toString());
+    //         console.log(numSessions)
+    //         if (numSessions <= 0) {
+    //             logout();
+    //             localStorage.setItem('sessions', "0");
+    //         } 
+    //     };
+    //     window.onunload = handleTabClose; //pra quando fechar a janela (ele não chama o return, de desmontar o componente)
+    //     return () => {
+    //         handleTabClose(); //pra quando desmontar o componente (não é chamado o 'onunload' ao dar refresh, mas sim o desmontar o compomente)
+    //     }
+    // }, []);
     
     return (
         <AuthContext.Provider value={value}>
