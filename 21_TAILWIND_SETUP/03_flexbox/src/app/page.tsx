19dd4255bb480910cc861105f73aa5a723236ebe
Alexandre B Coletti <alexandre.coletti@controlsoft.com.br>
2025-01-22 12:35:24 -0300

    finaliza aula de flex

--- 21_TAILWIND_SETUP/03_flexbox/src/app/page.tsx
+++ 21_TAILWIND_SETUP/03_flexbox/src/app/page.tsx
@@ -34,7 +34,37 @@ export default function Home() {
         <div className="bg-teal-600 p-6 m-2">4</div>
         <div className="bg-purple-600 p-6 m-2">5</div>
         <div className="bg-orange-600 p-6 m-2">6</div>
+      </div>
 
+      <div className="flex bg-red-300 flex-wrap">
+        <div className="bg-red-400 p-6">1</div>
+        <div className="bg-red-500 p-6">2</div>
+        <div className="bg-red-600 p-6">3</div>
+        <div className="bg-red-700 p-6">4</div>
+        <div className="bg-red-800 p-6">5</div>
+        <div className="bg-red-900 p-6">6</div>
+      </div>
+
+      <div className="flex bg-green-300 flex-wrap">
+        <div className="flex-1 bg-green-400 p-6">1</div>
+        <div className="flex-1 bg-green-500 p-6">2</div>
+        <div className="bg-green-600 p-6">3</div>
+        <div className="bg-green-700 p-6">4</div>
+      </div>
+
+      <div className="flex bg-purple-300 flex-wrap">
+        <div className="bg-purple-400 p-6 order-last">1</div>
+        <div className="bg-purple-500 p-6 order-4">2</div>
+        <div className="bg-purple-600 p-6 order-5">3</div>
+        <div className="bg-purple-700 p-6 order-2">4</div>
+        <div className="bg-purple-800 p-6 order-3">5</div>
+        <div className="bg-purple-900 p-6 order-first md:order-last">6</div>
+      </div>
+
+      <div className="container flex">
+        <div className="flex-1 bg-blue-300 p-6 order-1 lg:order-3">1</div>
+        <div className="flex-1 bg-blue-600 p-6 order-2">2</div>
+        <div className="flex-1 bg-blue-900 p-6 order-3 lg:order-1">3</div>
       </div>
 
       <p className="m-20"></p>
