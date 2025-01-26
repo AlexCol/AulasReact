1babeda3fc74b19f2e4b188f75c25f746087574d
AlexCol <eu_axil@yahoo.com.br>
2025-01-26 15:51:07 -0300

    .

---
+++ 21_TAILWIND_SETUP/06_spotify_clone/src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1 className="bg-red-900">Teste</h1>
+      <i className="fab fa-instagram"></i>
+    </div>
+  );
+}
---
+++ 21_TAILWIND_SETUP/06_spotify_clone/tailwind.config.ts
@@ -0,0 +1,38 @@
+import type { Config } from "tailwindcss";
+
+export default {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      spacing: {
+        '14': '3.5rem',
+        '125px': '125px'
+      },
+      colors: {
+        'hoverspt': '#18D760',
+        'purple-main': '#2D46B9',
+        'green-main': '#1ED760'
+      },
+      backgroundImage: theme => ({
+        'spotify-theme': "url('/public/imgs/bursts.svg')",
+        'spotify-theme-mobile': "url('/public/imgs/bursts-mobile.svg')",
+      }),
+      backgroundSize: {
+        '175%': '175%',
+        '195%': '195%',
+      },
+      backgroundPosition: {
+        'banner': '46% 4%',
+        'banner-mobile': 'top 25% center',
+      },
+      fontSize: {
+        '9xl': '9rem',
+      }
+    },
+  },
+  plugins: [],
+} satisfies Config;
